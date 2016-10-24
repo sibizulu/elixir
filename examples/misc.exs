308719795505c09ff617142236bc03e10faa6330
Light Server <sibiraj@lightrains.com>
2016-10-24 17:20:55 +0530

    Module example

---
+++ examples/misc.exs
@@ -0,0 +1,5 @@
+# Execute Math function in module file
+IO.puts Math.add(10, 2)
+IO.puts Math.sub(10, 2)
+IO.puts Math.mul(10, 2)
+IO.puts Math.div(10, 2)
---
+++ module/math.exs
@@ -0,0 +1,15 @@
+defmodule Math do
+  def add(a, b) do
+    a + b
+  end
+  def sub(a, b) do
+    a - b
+  end
+  def mul(a, b) do
+    a * b
+  end
+  def div(a, b) do
+    a / b
+  end
+end
+
